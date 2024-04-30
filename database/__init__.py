062318ac3a84e639630634191990d1a40c421781
jabyum <jabyum@github.com>
2024-04-30 16:44:17 +0500

    123

--- database/__init__.py
+++ database/__init__.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///data.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///data.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
